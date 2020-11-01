5d5f5cac95ef6b96976038f1fa3ec3191c117c22
Mat Watts <mathew.watts.dev@gmail.com>
2020-11-01 12:56:47 +1100

    Add BusinessList directory, BusinessList.css, BusinessList.js files, create new BusinessList component

---
+++ ravenous_react_app/src/components/BusinessList/BusinessList.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './BusinessList.css';
+// eslint-disable-next-line no-unused-vars
+import Business from '..Business/Business';
+
+class BusinessList extends React.Component {
+    render() {
+        return (
+<div className="BusinessList">
+  <Business />
+  <Business />
+  <Business />
+  <Business />
+  <Business />
+  <Business />
+</div>
+
+        )
+    }  
+}
+
+export default BusinessList; 
